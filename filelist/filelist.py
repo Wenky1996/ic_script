799a78062e3a5c665440f7d9147967dbbe7078fb
wenky <wenkyjong1996@gmail.com>
2024-07-30 23:24:46 +0800

    update filelist

--- filelist/filelist.py
+++ filelist/filelist.py
@@ -11,11 +11,16 @@
 import os
 
 current_folder = os.getcwd()
-file_list = open("rtl.f",'w+')
 
-for root,dir,files in os.walk(current_folder):
-    file_type = os.path.splitext(files)[1]
-    
+with open("rtl.f",'w') as file_list:
+    for root,dir,files in os.walk(current_folder):
+        for file in files:
+            file_type = os.path.splitext(file)[1]
+            if file_type == ".v" or file_type == ".sv" or file_type == ".h" or file_type == ".vh":
+                print(root)
+                print(file)
+                file_list.write(root+'/'+file+"\n")
+            else:
+                continue
 
-def __main__():
-    print("")
+print("filelist created ")
--- filelist/readme.md
+++ filelist/readme.md
@@ -1,4 +1,10 @@
 # IC Development scripts
 ## 1. filelist
-The filelist script is in order to generate file list files of current folders and subfolders.
-
+The filelist.py script is in order to generate file list files of current folders and subfolders.
+The supported file include .v .sv .h .vh extensions.
+First , create a command alias in the configuration file. Takinfg zshrc as example.
+```
+    alias filelist="python ~/script/python_script/filelist.py"
+```
+Then go to the directory and run the command *filelist*,you will get the result file named as *rtl.f* that is list of the file in the current folders and subfolders
+ ## 2. 
